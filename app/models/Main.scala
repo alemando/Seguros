02787235b53af20b72b6166d2397e97fa098871a
Eladio Salazar Suarez <esalazars@unal.edu.co>
2019-10-05 18:02:07 -0500

    Task 26
    Crear la clase Vendedor

--- app/models/Main.scala
+++ app/models/Main.scala
@@ -2,5 +2,8 @@ package models
 
 object Main{
   val tim = Cliente("12","Nombre")
+  val tom = Seller("123456789","Prueba", "Sujeto", "De", "5555")
+  
   Cliente.create(tim)
+  Seller.create(tom)
 }

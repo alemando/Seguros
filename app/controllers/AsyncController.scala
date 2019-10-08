f821e5fab5adb8329f22d64ee61dc20cfead26f9
oyeu <daarangovi@gmail.com>
2019-10-08 18:45:40 -0500

    precontrolador

--- app/controllers/AsyncController.scala
+++ app/controllers/AsyncController.scala
@@ -7,22 +7,32 @@ import play.api.mvc._
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.collection.mutable.ArrayBuffer
+import models.Cliente
 
 @Singleton
 class AsyncController @Inject()(cc: ControllerComponents, actorSystem: ActorSystem)(implicit exec: ExecutionContext) extends AbstractController(cc) {
 
   def verClientes = Action.async {
-    getFutureMessage(10.second).map { msg => Ok(views.html.clientes()) }
+    getFutureClientes(3.second).map { msg => Ok(views.html.clientes(msg)) }
   }
   
   def verAseguradoras = Action.async {
-    getFutureMessage(10.second).map { msg => Ok(msg) }
+    getFutureMessage(3.second).map { msg => Ok(msg) }
   }
   
   private def getFutureMessage(delayTime: FiniteDuration): Future[String] = {
     val promise: Promise[String] = Promise[String]()
     actorSystem.scheduler.scheduleOnce(delayTime) {
       promise.success("Hi!")
+    }(actorSystem.dispatcher) // run scheduled tasks using the actor system's dispatcher
+    promise.future
+  }
+  
+  private def getFutureClientes(delayTime: FiniteDuration): Future[ArrayBuffer[Cliente]] = {
+    val promise: Promise[ArrayBuffer[Cliente]] = Promise[ArrayBuffer[Cliente]]()
+    actorSystem.scheduler.scheduleOnce(delayTime) {
+      promise.success(ArrayBuffer(Cliente("holo","holo","holo","holo","holo","holo","holo","holo","holo","holo")))
     }(actorSystem.dispatcher) // run scheduled tasks using the actor system's dispatcher
     promise.future
   }
